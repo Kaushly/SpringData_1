336a5011ecd2ece2ae6e096f9580a50f3c355080
Каушлы Сергей <Kaushly@yandex.ru>
2021-01-14 02:42:22 +0300

    Lesson 7

---
+++ src/main/java/ru/geek/kaushly/product/GeekProductApplication.java
@@ -0,0 +1,13 @@
+package ru.geek.kaushly.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekProductApplication.class, args);
+	}
+
+}
